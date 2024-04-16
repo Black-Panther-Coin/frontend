5fc35e48e2ca79f5da5fe1f8f36e1a9342ef9591
Bright Mensah <bright.mensah.ext@innocv.com>
2024-04-16 11:04:07 +0000

    update on page responsiveness and text corrections

--- src/components/Buttons/navButtons.jsx
+++ src/components/Buttons/navButtons.jsx
@@ -2,13 +2,10 @@ import React from "react";
 
 const NavButtons = () => {
   return (
-    <div className="fixed top-0 left-0 right-0 flex justify-between items-center w-full p-4">
-      <button className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-3 px-6 rounded text-xl">
+    <div className="fixed top-0 left-0 right-0 flex flex-col md:flex-row justify-between items-center w-full p-4">
+      <button className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 md:py-3 md:px-6 rounded text-base md:text-lg mb-2 md:mb-0">
         Read ClawPaper
       </button>
-      {/* <button className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-3 px-6 rounded text-xl">
-        Connect Wallet
-      </button> */}
       <w3m-button />
     </div>
   );
--- src/pages/HomePage/homePage.jsx
+++ src/pages/HomePage/homePage.jsx
@@ -3,47 +3,48 @@ import { useSpring, animated } from "react-spring";
 import NavButtons from "../../components/Buttons/navButtons";
 import PantherLogo from "../../components/PantherLogo/panther";
 
-import { useAccount, useContractWrite} from 'wagmi'
-import { preSaleABI, preSaleAddress } from "../../constants/constants"
-import { parseEther } from 'viem'
-import toast, { Toaster } from 'react-hot-toast';
-import { useWaitForTransaction } from 'wagmi'
+import { useAccount, useContractWrite } from "wagmi";
+import { preSaleABI, preSaleAddress } from "../../constants/constants";
+import { parseEther } from "viem";
+import toast, { Toaster } from "react-hot-toast";
+import { useWaitForTransaction } from "wagmi";
 
 const HomePage = () => {
   const backgroundImageUrl = "/src/assets/images/bg.png";
-  const [value, setValue] = useState("")
+  const [value, setValue] = useState("");
 
-  const { address } = useAccount()
+  const { address } = useAccount();
 
   const { write, data: writeData } = useContractWrite({
     address: preSaleAddress,
     abi: preSaleABI,
-    functionName: 'buyToken',
-    onError:(error)=>{
-      console.log("Error", error)
-      toast.error("An Error Occurred while trying to buy PNTHR.")
-    }
-  })
-
-  const { isLoading } = useWaitForTransaction({ 
-    hash: writeData?.hash,
-    onSuccess:()=>{
-      toast.success("PNTHR bought successfully, check your wallet address.")
-      setValue("")
+    functionName: "buyToken",
+    onError: (error) => {
+      console.log("Error", error);
+      toast.error("An Error Occurred while trying to buy PNTHR.");
     },
-    onError:()=>toast.error("An Error Occurred")
-  })
+  });
+
+  const { isLoading } = useWaitForTransaction({
+    hash: writeData?.hash,
+    onSuccess: () => {
+      toast.success("PNTHR bought successfully, check your wallet address.");
+      setValue("");
+    },
+    onError: () => toast.error("An Error Occurred"),
+  });
 
   function buyToken() {
-    if(address && value !== "") {
-      write({ value: parseEther(value.toString()) })
+    if (address && value !== "") {
+      write({ value: parseEther(value.toString()) });
     } else {
-      toast.error("Ensure you are connected and you have entered a BNB amount.")
+      toast.error(
+        "Ensure you are connected and you have entered a BNB amount."
+      );
     }
   }
 
-  const label = isLoading ? "Buying.." : "Buy Now"
-
+  const label = isLoading ? "Buying.." : "Buy Now";
 
   // Define animations
   const fadeIn = useSpring({
@@ -66,38 +67,41 @@ const HomePage = () => {
 
   return (
     <animated.div
-    className="relative flex flex-col items-start justify-center min-h-screen w-full bg-gray-100 p-8 bg-cover bg-center font-montserrat"
-    style={{ ...fadeIn, backgroundImage: `url(${backgroundImageUrl})` }}
+      className="relative flex flex-col items-center justify-center min-h-screen w-full bg-gray-100 p-8 bg-cover bg-center font-montserrat md:items-start md:justify-start"
+      style={{ ...fadeIn, backgroundImage: `url(${backgroundImageUrl})` }}
     >
       <NavButtons />
-      <h1 className="text-4xl md:text-6xl lg:text-9xl leading-tight font-bold text-white text-left my-4">
+      <h1 className="text-4xl mt-12 md:text-6xl lg:text-9xl leading-tight font-bold text-white text-center md:text-left my-4">
         Black Panther Coin
       </h1>
-      <h1 className="text-4xl md:text-6xl lg:text-9xl leading-tight font-bold text-white text-left my-4">
+      <h1 className="text-4xl md:text-6xl lg:text-9xl leading-tight font-bold text-white text-center md:text-left my-4">
         Private Sale is <br /> Live
       </h1>
-      <p className="mt-4 text-base md:text-lg lg:text-2xl text-white text-left">
-        Participate in the private sale of the <br />{" "}
+      <p className="mt-4 text-base md:text-lg lg:text-2xl text-white text-center md:text-left">
+        Participate in the private sale of Black Panther ($PNTHR). <br />{" "}
         <span className="font-bold"> A MEME COIN WITH PURPOSE!</span> <br />{" "}
         Connect your wallet and buy Black Panther&nbsp;coin&nbsp;(PNTHR)
       </p>
       <animated.div
         style={buttonAnimation}
-        className="mt-4 flex flex-col md:flex-row items-start md:items-center"
+        className="mt-4 flex flex-col md:flex-row items-start md:items-center w-full"
       >
         <input
           value={value}
           onChange={(e) => setValue(e.target.value)}
           type="number"
           placeholder="Enter BNB"
-          className="bg-white text-black font-bold py-3 px-6 md:px-8 rounded border border-black my-2 md:my-0 md:mr-4 text-base md:text-lg lg:text-xl"
+          className="bg-white text-black font-bold py-3 px-6 md:px-8 rounded border border-black my-2 md:my-0 md:mr-4 text-base md:text-lg lg:text-xl w-full md:w-auto"
         />
-        <button onClick={buyToken} className="bg-white hover:bg-gray-200 text-black font-bold py-3 px-6 md:px-8 rounded md:my-0 text-base md:text-lg lg:text-xl">
+        <button
+          onClick={buyToken}
+          className="bg-yellow-500 hover:bg-gray-200 text-black font-bold py-3 px-6 md:px-8 rounded md:my-0 text-base md:text-lg lg:text-xl w-full md:w-auto"
+        >
           {label}
         </button>
       </animated.div>
       <button
-        className="bg-transparent hover:bg-white hover:text-black text-white font-bold py-3 px-6 md:px-8 rounded border border-white my-2 md:my-0 md:mr-4 text-base md:text-lg lg:text-xl"
+        className="bg-transparent hover:bg-white hover:text-black text-white font-bold py-3 px-6 md:px-8 rounded border border-yellow-500 my-2 md:my-0 md:mr-4 text-base md:text-lg lg:text-xl"
         style={{ marginTop: "20px" }}
       >
         View Contract
@@ -108,10 +112,10 @@ const HomePage = () => {
       >
         <PantherLogo className="w-12 h-12 md:w-16 md:h-16 lg:w-24 lg:h-24" />
         <span className="text-yellow-500 font-bold mt-2 text-base md:text-lg lg:text-xl">
-          BLACKPANTHER(PNTHR)
+          BLACKPANTHER ($PNTHR)
         </span>
       </animated.div>
-      <Toaster/>
+      <Toaster />
     </animated.div>
   );
 };
