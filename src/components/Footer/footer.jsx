7120e36818d9ac886e81bfa8d470afaebbfd908a
Bright Mensah <bright.mensah.ext@innocv.com>
2024-04-16 19:19:52 +0000

    added the footer section and made some changes to the navbar

---
+++ src/components/Footer/footer.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import bgImage from "../../assets/images/bg7.png";
+import Item1Image from "../../assets/images/doge-coin-logo-1024x1024.png";
+import Item2Image from "../../assets/images/shiba-inu-coin-logo.png";
+import Item3Image from "../../assets/images/pepe-coin-logo.png";
+
+const Footer = () => {
+  return (
+    <footer className="bg-gray-800 py-12 px-4 md:px-8 w-full relative">
+      <div
+        className="absolute top-0 left-0 w-full h-full"
+        style={{ backgroundImage: `url(${bgImage})`, backgroundSize: "cover" }}
+      />
+      <div className="container mx-auto relative z-10 flex flex-col items-start">
+        <div className="justify-between items-center">
+          {/* Dogecoin  Section */}
+          <div className="flex items-center mb-8">
+            <img src={Item1Image} alt="Dogecoin" className="w-40 h-auto mr-4" />
+            <div>
+              <h1 className="text-4xl md:text-5xl font-bold text-white mb-2">
+                From Jobless to Millionaire!
+              </h1>
+            </div>
+          </div>
+
+          {/* Shiba Section */}
+          <div className="flex items-center mb-8">
+            <img src={Item2Image} alt="Shiba " className="w-40 h-auto mr-4" />
+            <div>
+              <h1 className="text-4xl md:text-5xl font-bold text-white mb-2">
+                Dogecoin Millionaire in 2 Months
+              </h1>
+            </div>
+          </div>
+
+          {/* Pepe Section */}
+          <div className="flex items-center mb-8">
+            <img src={Item3Image} alt="Pepe" className="w-40 h-auto mr-4" />
+            <div>
+              <h1 className="text-4xl md:text-5xl font-bold text-white mb-2">
+                $27 Turns Into 1Million
+              </h1>
+            </div>
+          </div>
+        </div>
+        {/* Footer Bottom Section */}
+        <div className="text-center mt-8 items-center">
+          <p className="text-white">
+            &copy; {new Date().getFullYear()} Black Panther Coin. All rights
+            reserved.
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/pages/HomePage/homePage.jsx
+++ src/pages/HomePage/homePage.jsx
@@ -8,6 +8,7 @@ import { preSaleABI, preSaleAddress } from "../../constants/constants";
 import { parseEther } from "viem";
 import toast, { Toaster } from "react-hot-toast";
 import { useWaitForTransaction } from "wagmi";
+import Footer from "../../components/Footer/footer";
 
 const HomePage = () => {
   const backgroundImageUrl = "/src/assets/images/bg.png";
@@ -66,63 +67,67 @@ const HomePage = () => {
   });
 
   return (
-    <animated.div
-      className="relative flex flex-col items-center justify-center min-h-screen w-full bg-gray-100 p-8 bg-cover bg-center font-montserrat md:items-start md:justify-start"
-      style={{ ...fadeIn, backgroundImage: `url(${backgroundImageUrl})` }}
-    >
-      <NavButtons />
-      <h1 className="text-4xl mt-12 md:text-6xl lg:text-9xl leading-tight font-bold text-white text-center md:text-left my-4">
-        Black Panther Coin
-      </h1>
-      <h1 className="text-4xl md:text-6xl lg:text-9xl leading-tight font-bold text-white text-center md:text-left my-4">
-        Private Sale is <br /> Live
-      </h1>
-      <p className="mt-4 text-base md:text-lg lg:text-2xl text-white text-center md:text-left">
-        Participate in the private sale of{" "}
-        <span className="font-bold">Black Panther</span>{" "}
-        <span className="text-yellow-500 font-bold">($PNTHR)</span>. <br />{" "}
-        <span className="font-bold">
-          {" "}
-          <span className="text-yellow-500"> A MEME COIN WITH PURPOSE!</span>
-        </span>{" "}
-        <br /> Connect your wallet and buy{" "}
-        <span className="font-bold">Black Panther</span>&nbsp;coin&nbsp;(PNTHR)
-      </p>
+    <>
       <animated.div
-        style={buttonAnimation}
-        className="mt-4 flex flex-col md:flex-row items-start md:items-center w-full"
+        className="relative flex flex-col items-center justify-center min-h-screen w-full bg-gray-100 p-8 bg-cover bg-center font-montserrat md:items-start md:justify-start"
+        style={{ ...fadeIn, backgroundImage: `url(${backgroundImageUrl})` }}
       >
-        <input
-          value={value}
-          onChange={(e) => setValue(e.target.value)}
-          type="number"
-          placeholder="Enter BNB"
-          className="bg-white text-black font-bold py-3 px-6 md:px-8 rounded border border-black my-2 md:my-0 md:mr-4 text-base md:text-lg lg:text-xl w-full md:w-auto"
-        />
-        <button
-          onClick={buyToken}
-          className="bg-yellow-500 hover:bg-gray-200 text-black font-bold py-3 px-6 md:px-8 rounded md:my-0 text-base md:text-lg lg:text-xl w-full md:w-auto"
+        <NavButtons />
+        <h1 className="text-4xl mt-12 md:text-6xl lg:text-9xl leading-tight font-bold text-white text-center md:text-left my-4">
+          Black Panther Coin
+        </h1>
+        <h1 className="text-4xl md:text-6xl lg:text-9xl leading-tight font-bold text-white text-center md:text-left my-4">
+          Private Sale is <br /> Live
+        </h1>
+        <p className="mt-4 text-base md:text-lg lg:text-2xl text-white text-center md:text-left">
+          Participate in the private sale of{" "}
+          <span className="font-bold">Black Panther</span>{" "}
+          <span className="text-yellow-500 font-bold">($PNTHR)</span>. <br />{" "}
+          <span className="font-bold">
+            {" "}
+            <span className="text-yellow-500"> A MEME COIN WITH PURPOSE!</span>
+          </span>{" "}
+          <br /> Connect your wallet and buy{" "}
+          <span className="font-bold">Black Panther</span>
+          &nbsp;coin&nbsp;(PNTHR)
+        </p>
+        <animated.div
+          style={buttonAnimation}
+          className="mt-4 flex flex-col md:flex-row items-start md:items-center w-full"
         >
-          {label}
+          <input
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+            type="number"
+            placeholder="Enter BNB"
+            className="bg-white text-black font-bold py-3 px-6 md:px-8 rounded border border-black my-2 md:my-0 md:mr-4 text-base md:text-lg lg:text-xl w-full md:w-auto"
+          />
+          <button
+            onClick={buyToken}
+            className="bg-yellow-500 hover:bg-gray-200 text-black font-bold py-3 px-6 md:px-8 rounded md:my-0 text-base md:text-lg lg:text-xl w-full md:w-auto"
+          >
+            {label}
+          </button>
+        </animated.div>
+        <button
+          className="bg-transparent hover:bg-white hover:text-black text-white font-bold py-3 px-6 md:px-8 rounded border border-yellow-500 my-2 md:my-0 md:mr-4 text-base md:text-lg lg:text-xl"
+          style={{ marginTop: "20px" }}
+        >
+          View Contract
         </button>
+        <animated.div
+          style={logoAnimation}
+          className="absolute bottom-0 right-0 mb-8 mr-8 flex flex-col items-center"
+        >
+          <PantherLogo className="w-12 h-12 md:w-16 md:h-16 lg:w-24 lg:h-24" />
+          <span className="text-yellow-500 font-bold mt-2 text-base md:text-lg lg:text-xl">
+            BLACK PANTHER ($PNTHR)
+          </span>
+        </animated.div>
+        <Toaster />
       </animated.div>
-      <button
-        className="bg-transparent hover:bg-white hover:text-black text-white font-bold py-3 px-6 md:px-8 rounded border border-yellow-500 my-2 md:my-0 md:mr-4 text-base md:text-lg lg:text-xl"
-        style={{ marginTop: "20px" }}
-      >
-        View Contract
-      </button>
-      <animated.div
-        style={logoAnimation}
-        className="absolute bottom-0 right-0 mb-8 mr-8 flex flex-col items-center"
-      >
-        <PantherLogo className="w-12 h-12 md:w-16 md:h-16 lg:w-24 lg:h-24" />
-        <span className="text-yellow-500 font-bold mt-2 text-base md:text-lg lg:text-xl">
-          BLACK PANTHER ($PNTHR)
-        </span>
-      </animated.div>
-      <Toaster />
-    </animated.div>
+      <Footer />
+    </>
   );
 };
 
