bc1cc3202a4d380fc36a4f0a78a3133155c51b4a
divinejicks <divinejicks@gmail.com>
2024-05-01 14:33:01 +0100

    change

--- src/components/Popups/propUps.jsx
+++ src/components/Popups/propUps.jsx
@@ -4,8 +4,8 @@ import React from "react";
 const propUps = ({ onClose }) => {
   return (
     <div className="fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center z-50">
-      <div className="p-6 bg-gray-900 rounded-md shadow-lg max-w-sm text-center">
-        <p className="text-lg font-semibold text-yellow-500">
+      <div className="p-6 rounded-md shadow-lg max-w-sm text-center" style={{backgroundColor: "#F8F5ED"}}>
+        <p className="text-lg font-semibold text-black">
           “Note: $BPNTHR tokens bought during the discounted Private Sale period
           are subject to a 20% fee if sold within the first 60 days of the
           project Go-live.
