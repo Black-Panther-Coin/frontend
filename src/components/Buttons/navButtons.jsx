01e5f952a1d5240644f0fde85644969a445f9455
divinejicks <divinejicks@gmail.com>
2024-05-02 11:25:24 +0100

    haha

--- src/components/Buttons/navButtons.jsx
+++ src/components/Buttons/navButtons.jsx
@@ -53,6 +53,13 @@ const NavButtons = () => {
                 >
                   Read ClawPaper
                 </a>
+                <a
+                  className="text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center cursor-pointer"
+                  onClick={() => window.open("https://bscscan.com/address/0x12a55f6aBDfE13a44eF8b29a24964e20D21E0fA5", "_blank")}
+                  aria-current="page"
+                >
+                  View Contract
+                </a>
               </li>
             </ul>
         </div>
