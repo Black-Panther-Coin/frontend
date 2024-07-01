704d675db59a11a826622128c80a02bd059b35fd
Mbuh Divine <divinejicks@gmail.com>
2024-07-01 09:59:22 +0100

    removed comented code

--- src/components/httpClient/httpClient.js
+++ src/components/httpClient/httpClient.js
@@ -1,26 +1,13 @@
-// import axios from "axios";
-// import AuthInterceptor from "../interceptors/auth-interceptor";
-// import ErrorInterceptor from "../interceptors/error-interceptor";
-
-// const httpClient = axios.create({
-//     baseURL: import.meta.env.VITE_API_URL 
-// });
-
-// AuthInterceptor(httpClient)
-// ErrorInterceptor(httpClient)
-
-// export default httpClient;
-
-
 import axios from "axios";
 import AuthInterceptor from "../interceptors/auth-interceptor";
 import ErrorInterceptor from "../interceptors/error-interceptor";
 
 const httpClient = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+    baseURL: import.meta.env.VITE_API_URL 
 });
 
-AuthInterceptor(httpClient);
-ErrorInterceptor(httpClient);
+AuthInterceptor(httpClient)
+ErrorInterceptor(httpClient)
 
 export default httpClient;
+
--- src/components/interceptors/auth-interceptor.js
+++ src/components/interceptors/auth-interceptor.js
@@ -1,34 +1,18 @@
-// const AuthInterceptor = (axiosInstance) => {
-
-//     axiosInstance.interceptors.request.use(
-//         req => {
-//             const token = localStorage.getItem('token_key_Enk')
-//             if(token) {
-//                 req.headers['Authorization'] = "Bearer " + token;
-//             }
-//             return req;
-//         },
-//         err => {return Promise.reject(err)}
-//     )
-// }
-
-// export default AuthInterceptor;
-
-
-
-
 const AuthInterceptor = (axiosInstance) => {
+
     axiosInstance.interceptors.request.use(
-      (req) => {
-        const token = localStorage.getItem("token_key_Bpnthr");
-        if (token) {
-          req.headers["Authorization"] = "Bearer " + token;
-        }
-        return req;
-      },
-      (err) => Promise.reject(err)
-    );
-  };
-  
-  export default AuthInterceptor;
+        req => {
+            const token = localStorage.getItem('token_key_Enk')
+            if(token) {
+                req.headers['Authorization'] = "Bearer " + token;
+            }
+            return req;
+        },
+        err => {return Promise.reject(err)}
+    )
+}
+
+export default AuthInterceptor;
+
+
   
