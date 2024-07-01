8381d60652d9df3bbdf4938fcc14c18cad2d6ff5
Mbuh Divine <divinejicks@gmail.com>
2024-07-01 10:37:55 +0100

    stable coins

--- src/pages/HomePage/landingPage.jsx
+++ src/pages/HomePage/landingPage.jsx
@@ -156,6 +156,7 @@ const Header = () => {
           gateway: {
             managed: { methods: [
               { network: 'SepoliaTestnet', token: null, discountPercentOff: 0 }, // show this for testing
+              { network: 'BNB', token: null, discountPercentOff: 0 },
               { network: 'BNB', token: "0xe9e7cea3dedca5984780bafc599bd69add087d56", discountPercentOff: 0 },
               { network: 'BNB', token: "0x55d398326f99059ff775485246999027b3197955", discountPercentOff: 0 },
               { network: 'BNB', token: "0x8ac76a51cc950d9822d68b83fe1ad97b32cd580d", discountPercentOff: 0 }
